a3309be326a032ade98b441ded6d75b0165b41ed
haixu <85544991+haixu48@users.noreply.github.com>
2021-06-09 21:20:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc : Thường lệ vốn trái đất
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    Lạc vào tim anh <33
+    Đằng sau chữ yêu đấy là thương`,
+    btnIntro: 'nhấn để tiếp tục.hi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo : 'Không, Anh mơ à: 3' ,
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
